db45ed7cc51b6a44ff5e17fbdbb48cf91690a820
abhishek <abhishek@codeblues.in>
2012-07-12 01:17:14 +0530

    Corrected a bug in minify.bat

--- js/Globals.js
+++
@@ -1,1 +0,0 @@
-var subscriptionTimer,inSubscriptionState=!1,loadingFinished=!0,fetchTimer,inFetchingState=!1,STACK_SIZE=2E3,SHOWALL=1,SHOWUNREAD=0,NO_OF_FEEDS=20,IS_SYNC_ON=!1;
--- minify.bat
+++ minify.bat
@@ -1,16 +1,23 @@
-java -jar compiler.jar --js js/background.js --js_output_file compiled/background.js
-java -jar compiler.jar --js js/DbManager.js --js_output_file compiled/DbManager.js
-java -jar compiler.jar --js js/FeedViewer.js --js_output_file compiled/FeedViewer.js
-java -jar compiler.jar --js js/Globals.js --js_output_file compiled/Globals.js
-java -jar compiler.jar --js js/GoogleReader.js --js_output_file compiled/GoogleReader.js
-java -jar compiler.jar --js js/popup.js --js_output_file compiled/popup.js
-java -jar compiler.jar --js js/ReaderController.js --js_output_file compiled/ReaderController.js
-java -jar compiler.jar --js js/ReaderViewer.js --js_output_file compiled/ReaderViewer.js
-java -jar compiler.jar --js js/Utils.js --js_output_file compiled/Utils.js
-java -jar compiler.jar --js js/YoutubeManager.js --js_output_file compiled/YoutubeManager.js
-java -jar compiler.jar --js js/lib/GAPokki.js --js_output_file compiled/lib/GAPokki.js
-java -jar compiler.jar --js js/lib/jquery.anythingslider.js --js_output_file compiled/lib/jquery.anythingslider.js
-java -jar compiler.jar --js js/lib/jquery.easing.1.3.js --js_output_file compiled/lib/jquery.easing.1.3.js
-java -jar compiler.jar --js js/lib/LocalStore.js --js_output_file compiled/lib/LocalStore.js
-java -jar compiler.jar --js js/lib/PokkiBrowser.js --js_output_file compiled/lib/PokkiBrowser.js
-java -jar compiler.jar --js js/lib/SmoothScroll.js --js_output_file compiled/lib/SmoothScroll.js
+rm -r js
+mkdir js
+cd js
+mkdir lib
+cd ..
+cp js_src/background.js  js/background.js
+java -jar compiler.jar --js js_src/DbManager.js --js_output_file js/DbManager.js
+java -jar compiler.jar --js js_src/FeedViewer.js --js_output_file js/FeedViewer.js
+java -jar compiler.jar --js js_src/Globals.js --js_output_file js/Globals.js
+java -jar compiler.jar --js js_src/GoogleReader.js --js_output_file js/GoogleReader.js
+java -jar compiler.jar --js js_src/popup.js --js_output_file js/popup.js
+java -jar compiler.jar --js js_src/ReaderController.js --js_output_file js/ReaderController.js
+java -jar compiler.jar --js js_src/ReaderViewer.js --js_output_file js/ReaderViewer.js
+java -jar compiler.jar --js js_src/Utils.js --js_output_file js/Utils.js
+cp js_src/YoutubeManager.js  compiled/YoutubeManager.js
+java -jar compiler.jar --js js_src/lib/GAPokki.js --js_output_file js/lib/GAPokki.js
+java -jar compiler.jar --js js_src/lib/jquery.anythingslider.js --js_output_file js/lib/jquery.anythingslider.js
+java -jar compiler.jar --js js_src/lib/jquery.easing.1.3.js --js_output_file js/lib/jquery.easing.1.3.js
+java -jar compiler.jar --js js_src/lib/LocalStore.js --js_output_file js/lib/LocalStore.js
+java -jar compiler.jar --js js_src/lib/PokkiBrowser.js --js_output_file js/lib/PokkiBrowser.js
+java -jar compiler.jar --js js_src/lib/SmoothScroll.js --js_output_file js/lib/SmoothScroll.js
+cp js_src/lib/jquery.min.js js/lib/jquery.min.js
+cp js_src/lib/jQuery.jQTubeUtil.js js/lib/jQuery.jQTubeUtil.js
